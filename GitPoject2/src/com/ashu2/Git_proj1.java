faaa02c18a247466806f8703b934a35cf2ebed93
akash <akhouriakash10@gmail.com>
2024-09-16 23:22:32 +0530

    commit update 2

---
+++ GitPoject2/src/com/ashu2/Git_proj1.java
@@ -0,0 +1,10 @@
+package com.ashu2;
+
+public class Git_proj1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this one is my second project");
+	}
+
+}
